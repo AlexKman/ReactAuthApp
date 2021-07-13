b8244a3326961b2b2c173214b7597f015f8fd357
Alex Keeley <keeley.lp@gmail.com>
2021-07-13 18:45:42 +0100

    Updated dashboard page with logout button and a preliminary endpoint to update your profile

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,11 +1,23 @@
 import React, { useState } from "react";
 import { Card, Button, Alert } from "react-bootstrap";
+import { Link, useHistory } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
 
 export default function Dashboard() {
   const [error, setError] = useState();
-  const { currentUser } = useAuth();
-  function handleLogout() {}
+  const { currentUser, logout } = useAuth();
+  const history = useHistory();
+
+  async function handleLogout() {
+    setError("");
+
+    try {
+      await logout();
+      history.push("/login");
+    } catch {
+      setError("Failed to log out");
+    }
+  }
 
   return (
     <div>
@@ -15,6 +27,9 @@ export default function Dashboard() {
           <h2 className="text-center mb-4">Profile</h2>
           {error && <Alert variant="danger">{error}</Alert>}
           <strong>Email: {currentUser.email}</strong>
+          <Link to="/updateprofile" className="btn btn-primary w-100">
+            Update profile
+          </Link>
         </Card.Body>
       </Card>
       <div className="w-100 text-center mt-2">
--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -18,6 +18,9 @@ export function AuthProvider({ children }) {
   function login(email, password) {
     return auth.signInWithEmailAndPassword(email, password);
   }
+  function logout() {
+    return auth.signOut();
+  }
 
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged((user) => {
@@ -27,7 +30,7 @@ export function AuthProvider({ children }) {
     return unsubscribe;
   }, []);
 
-  const value = { currentUser, signUp, login };
+  const value = { currentUser, signUp, login, logout };
 
   return (
     <AuthContext.Provider value={value}>
